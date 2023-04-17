972aa31049290831043ce1d02a5fc3f092c5c0c2
14H <doduchieu.kstn@gmail.com>
2023-04-17 11:51:19 +0700

    update prompt.dart

--- lib/domain/remote_config/model/prompt.dart
+++ lib/domain/remote_config/model/prompt.dart
@@ -2,26 +2,24 @@ import 'package:ai_girl_friends/domain/remote_config/model/System_prompt.dart';
 
 class Prompt extends SystemPrompt {
   final int id;
-  final int characterId;
-  final String content;
   final String randomContent;
   final int milestone;
 
   Prompt({
     required this.id,
-    required this.characterId,
-    required this.content,
+    required super.characterId,
+    required super.content,
     required this.randomContent,
     required this.milestone,
   });
 
   Prompt.fromJson(dynamic json)
       : id = json['id'],
-        characterId = json['character_id'],
-        content = json['content'],
         randomContent = json['random_content'],
-        milestone = json['milestone'];
+        milestone = json['milestone'],
+        super.fromJson(json);
 
+  @override
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['id'] = id;
